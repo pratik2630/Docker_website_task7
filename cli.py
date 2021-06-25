77342dddd5cb6bde6a03942f104ef335af44d4d5
pratik2630 <GitHub@#2630>
2021-06-25 23:06:38 +0530

    DockerWebsite.v:01

---
+++ cli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+import time
+
+
+print("content-type: text/html")
+print()
+
+
+f = cgi.FieldStorage()
+cmd = f.getvalue("x")
+
+o = subprocess.getoutput("sudo " + cmd)
+print(o)
---
+++ gui.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+print("content-type: text/html")
+print()
+
+
+import subprocess as sb
+import cgi
+
+print("Welcome")
+var = cgi.FieldStorage("command")
+command = var.getvalue("command")
+output = sb.getoutput("sudo " + command)
+print(output)
